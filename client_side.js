f3997eb1b3d26e934b72d0ae55279c3ee2968595
RAJKAMAL KUMAR <rajkamalkumar207@gmail.com>
2021-06-29 19:15:20 +0530

    Change titles and styling on homepage

---
+++ client_side.js
@@ -0,0 +1,135 @@
+
+   
+    const t20SR = document.querySelector('.t20SR');
+    const t20run = document.querySelector('.t20run');
+    const t20hundred = document.querySelector('.t20hundred');
+    const t20fifty = document.querySelector('.t20fifty');
+    const t20highscore = document.querySelector('.t20highscore');
+
+
+
+    const odiSR = document.querySelector('.odiSR');
+    const odirun = document.querySelector('.odirun');
+    const odihundred = document.querySelector('.odihundred');
+    const odififty = document.querySelector('.odififty');
+    const odihighscore = document.querySelector('.odihighscore');
+
+    const testSR = document.querySelector('.testSR');
+       const testrun = document.querySelector('.testrun');
+       const testhundred = document.querySelector('.testhundred');
+       const testfifty = document.querySelector('.testfifty');
+       const highscore = document.querySelector('.highscore');
+  
+    const playingRole = document.querySelector('.playingRole');
+    const fullName = document.querySelector('.fullName');
+    
+    const weatherForm = document.querySelector('form')
+    const name = document.querySelector('.name');
+    const born = document.querySelector('.born');
+    const currentAge  = document.querySelector('.currentAge');
+    const majorTeams  = document.querySelector('.majorTeams')
+    const bowlingStyle  = document.querySelector('.bowlingStyle')
+    const battingstyle = document.querySelector('#battingstyle')
+ 
+    const pidvalue = document.querySelector('#pidvalue')
+
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+const img1= document.querySelector('img')
+const profile= document.querySelector('.profile')
+const odiwicket= document.querySelector('.odiwicket')
+const t20wicket= document.querySelector('.t20wicket')
+const testwicket= document.querySelector('.testwicket')
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value;
+console.log(location);
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/player?name='+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent =data.country;
+                messageTwo.textContent =data.pid;
+                pidvalue.textContent=data.pid;
+    profile.textContent=data.profile;
+    if(data.data.bowling.ODIs)
+    {
+        odiwicket.textContent=data.data.bowling.ODIs.Wkts;
+    console.log("odi"+data.data.bowling.ODIs.Wkts);
+    }
+    if(data.data.bowling.T20Is)
+    { t20wicket.textContent=data.data.bowling.T20Is.Wkts;
+    console.log("t20"+data.data.bowling.T20Is.Wkts);
+    }
+    if(data.data.bowling.tests)
+    {
+        testwicket.textContent=data.data.bowling.tests.Wkts;
+    console.log("test"+data.data.bowling.tests.Wkts);
+    }
+    console.log("runs");
+
+
+
+
+
+
+
+
+    if(data.data.batting.tests)
+    { testSR.textContent=data.data.batting.tests.SR;
+        highscore.textContent=data.data.batting.tests.HS;
+        testrun.textContent=data.data.batting.tests.Runs;
+        testfifty.textContent=data.data.batting.tests["50"];
+        testhundred.textContent=data.data.batting.tests["100"];
+      console.log("test"+data.data.batting.tests.Runs);
+    }
+      if(data.data.batting.ODIs)
+      {odififty.textContent=data.data.batting.ODIs["50"];
+      odihighscore.textContent=data.data.batting.ODIs.HS;
+      odiSR.textContent=data.data.batting.ODIs.SR;
+          odirun.textContent=data.data.batting.ODIs.Runs;
+          odihundred.textContent=data.data.batting.ODIs["100"];
+    console.log("odi"+data.data.batting.ODIs.Runs);
+      }
+
+
+
+    if(data.data.batting.T20Is)
+    { t20run.textContent=data.data.batting.T20Is.Runs;
+        t20highscore.textContent=data.data.batting.T20Is.HS;
+        t20SR.textContent=data.data.batting.T20Is.SR;
+        t20fifty.textContent=data.data.batting.T20Is["50"]; 
+        t20hundred.textContent=data.data.batting.T20Is["100"];
+    console.log("t20"+data.data.batting.T20Is.Runs);
+
+    }
+
+              //console.log("profilehaibe madharchodjd"+data.profile);
+              /*let result = data.data.batting.hasOwnProperty(T20Is);
+              console.log(result);*/
+            //  console.log("bowling    "+data.data.batting.T20Is.Runs);
+              battingstyle.textContent=data.battingStyle;
+              
+              bowlingStyle.textContent=data.bowlingStyle;
+              majorTeams.textContent=data.majorTeams;
+              currentAge.textContent=data.currentAge;
+              born.textContent=data.born;
+              if(data.imageURL!=null)
+               img1.src=data.imageURL;
+
+playingRole.textContent=data.playingRole;
+fullName.textContent=data.fullName;
+name.textContent=data.name;
+console.log("profile"+ data.profile);
+       // console.log(data.data[0].bowling.listA);   
+            }
+        })
+    })
+})
+    
---
+++ server.js
@@ -0,0 +1,125 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+//const geocode = require('./geo');
+//const forecast = require('./forr');
+const search= require('./testing1')
+const psearch =require('./testing')
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname)
+const viewsPath = path.join(__dirname)
+const partialsPath = path.join(__dirname)
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index1', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+app.get('/home', (req, res) => {
+    res.render('index1', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/player', (req, res) => {
+    if (!req.query.name) {
+        return res.send({
+            error: 'You must provide an name!'
+        })
+    }
+console.log(req.query.name);
+
+search(req.query.name, (error, res1) => {
+    if (error) {
+        return console.log({error})
+    }
+console.log("pid value "+res);
+    psearch(res1, (error, playerinfo) => {
+        if (error) {
+            return console.log({error})
+        }
+          else
+
+{ console.log(playerinfo);
+
+console.log(playerinfo.data);
+res.send(
+     playerinfo
+  
+)
+        console.log('pid value'+playerinfo.pid);
+        
+        console.log(' country  '+playerinfo.country);
+
+}
+    })
+})
+
+
+})
+
+
+
+
+/*
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+*/
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
+
+
